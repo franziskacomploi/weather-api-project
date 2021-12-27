e865674d302e0b3035db7f81a4ce4edae21f9a03
Franziska Pendzialek <franziska.pendzialek@outlook.com>
2021-12-27 12:22:29 +0100

    add basic styling and layout

---
+++ weather-project/src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <h1 className="p-8 text-center font-bold text-3xl">
+    <span className="font-light">Weather</span> Forecast
+  </h1>
+);
+
+export default Header;
